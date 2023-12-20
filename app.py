2c8dc80f45e30b2f8341654dba773d9f9830334a
Alpina Damayanti <72603405+alpina-datascience@users.noreply.github.com>
2023-12-21 04:36:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,50 @@
 import streamlit as st
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
+from PIL import Image
 import numpy as np
+import time
 
 # Load the saved model
-model = tf.keras.models.load_model('model.h5')
+try:
+    model = tf.keras.models.load_model('model.h5')
+except Exception as e:
+    st.write("Error loading the model:")
+    st.write(str(e))  # Display the specific error message for debugging
 
 st.title("Historical Building Cracks Detection")
 
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...")
 
 if uploaded_file is not None:
     # Display the selected image
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    img = Image.open(uploaded_file)
+    st.image(img, caption='Uploaded Image.', use_column_width=True)
 
-    # Preprocess the image
-    img_array = image.img_to_array(image)
+    # Resize the image to match the expected input shape of the model (150x150)
+    img_resized = img.resize((150, 150))
+
+    # Preprocess the resized image
+    img_array = image.img_to_array(img_resized)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
+    img_array = img_array / 255.0  # Normalize pixel values if required by the model
 
-    # Make prediction
+    # Make prediction and measure time
+    start_time = time.time()
     prediction = model.predict(img_array)
+    end_time = time.time()
 
-    # Display the result
-    if prediction[0][0] > 0.5:
-        st.write("Prediction: Cracked")
+    # Calculate accuracy
+    confidence = prediction[0][0]
+    if confidence > 0.5:
+        accuracy = confidence * 100
     else:
-        st.write("Prediction: Not Cracked")
+        accuracy = (1 - confidence) * 100
+
+    # Ensure accuracy stays within 0-100% range
+    accuracy = min(95, max(0, accuracy))
+
+    # Display the result including accuracy and execution time
+    st.write(f"Prediction: {'Not Cracked' if confidence > 0.5 else 'Cracked'}")
+    st.write(f"Accuracy: {accuracy:.2f}%")
+    st.write(f"Execution Time: {end_time - start_time:.5f} seconds")
